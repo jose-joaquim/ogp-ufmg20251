37c01cea1d8946aaa765165eac6af771100777a9
José Joaquim de Andrade Neto <jjandradent@gmail.com>
2025-04-23 22:40:40 -0300

    (wip) Implementing trivial model

--- 2nd-exam/question2.py
+++ 2nd-exam/question2.py
@@ -32,7 +32,9 @@ class CData:
             self.transhipment_nodes = []
             self.arcs = {}
             self.arcs_fixed_cost = {}
+            self.node_requirement = {}
             for node in inst["nodes"]:
+                self.node_requirement[node[0]] = node[1]
                 if node[1] < 0:
                     self.source_nodes[node[0]] = node[1]
                 elif node[1] > 0:
@@ -140,8 +142,6 @@ class flow_model_improved:
         g_ijk, q_jk, arc_activation = variables(model)
         constraints(model, g_ijk, q_jk, arc_activation)
 
-        model.write("improved.lp")
-
         self.model, self.arc_activation, self.g_ijk, self.q_jk = (
             model,
             arc_activation,
@@ -149,45 +149,63 @@ class flow_model_improved:
             q_jk,
         )
 
+    def run(self, relax, verbose=False, show_status=False, lp_name=None):
+        self.model.verbose = verbose
+        self.model.optimize(relax=relax, max_seconds=600)
+        if lp_name is not None:
+            self.model.write(lp_name)
+
+        if show_status is True:
+            print(f"-- Optimal objective {self.model.objective_value}")
+
 
 class flow_model:
     def __init__(self, dt):
         self.dt = dt
+        self.arcs = dt.arcs
+        self.arcs_fixed_cost = dt.arcs_fixed_cost
+        self.demand_nodes = dt.demand_nodes
+        self.source_nodes = dt.source_nodes
+        self.N = dt.N
+        self.node_requirement = dt.node_requirement
         self.create_model()
 
     def create_model(self):
-        def variables(model, A, c):
-            edge_activation = {
+        def variables(model):
+            arc_activation = {
                 (i, j): model.add_var(
-                    "x({},{})".format(i, j), var_type=BINARY, obj=c[i][j]
+                    "x({},{})".format(i, j),
+                    var_type=BINARY,
+                    obj=self.arcs_fixed_cost[i, j],
                 )
-                for (i, j) in A
+                for (i, j) in self.arcs
             }
 
-            edge_flow = {
-                (i, j): model.add_var("f({},{})".format(i, j), lb=0.0) for (i, j) in A
+            arc_flow = {
+                (i, j): model.add_var(
+                    "f({},{})".format(i, j), lb=0.0, obj=self.arcs[i, j]
+                )
+                for (i, j) in self.arcs
             }
 
-            return edge_activation, edge_flow
+            return arc_activation, arc_flow
 
-        def constraints(
-            model, edge_activation, edge_flow, A, n, flow_req, demand_nodes
-        ):
-            for j in range(10):
-                model += xsum() - xsum() == flow_req[j]
-
-            for i, j in A:
+        def constraints(model, arc_activation, arc_flow):
+            for j in range(self.N):
                 model += (
-                    edge_flow[i, j]
-                    <= max(demand_nodes.values()) * edge_activation[i, j]
+                    xsum(arc_flow[i, jj] for (i, jj) in self.arcs if jj == j)
+                    - xsum(arc_flow[jj, i] for (jj, i) in self.arcs if jj == j)
+                    == self.node_requirement[j]
                 )
 
-        dt = self.dt
-        A, n, c = dt.A, dt.n, dt.c
+            big_D = sum(self.demand_nodes.values())
+            for i, j in self.arcs:
+                model += arc_flow[i, j] <= big_D * arc_activation[i, j]
+
         model = Model(sense=MINIMIZE, solver_name=GUROBI)
 
-        edge_activation, edge_flow = variables(model, A, c)
-        constraints(model, edge_activation, edge_flow, A, n)
+        edge_activation, edge_flow = variables(model)
+        constraints(model, edge_activation, edge_flow)
 
         self.model, self.edge_activation, self.edge_flow = (
             model,
@@ -209,3 +227,7 @@ if __name__ == "__main__":
     seed = 10
     dt = CData()
     improved = flow_model_improved(dt)
+    improved.run(relax=False, verbose=True)
+
+    trivial = flow_model(dt)
+    trivial.run(relax=False, verbose=True)
